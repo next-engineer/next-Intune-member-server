4219fdaf5def8d525765dd9eee33dc7513982d10
cw7430 <cw282@naver.com>
2025-08-12 10:50:55 +0900

    signIn

--- src/main/java/com/next/intune/user/controller/UserController.java
+++ src/main/java/com/next/intune/user/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.next.intune.user.controller;
 
+import com.next.intune.common.api.ApiResult;
+import com.next.intune.user.dto.request.SignInRequestDto;
 import com.next.intune.user.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,4 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
+    @PostMapping("/sign-in")
+    public ResponseEntity<ApiResult<?>> signIn(HttpServletResponse response, @Valid @RequestBody SignInRequestDto dto) {
+        userService.signIn(response, dto);
+        return ResponseEntity.ok(ApiResult.success());
+    }
 }
