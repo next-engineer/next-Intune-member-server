ce54725508c31c1ceabce29230188342b067eae7
YOUNGJI KIM <yjkim7885@knou.ac.kr>
2025-08-12 16:08:44 +0900

    implement sign-up services

--- src/main/java/com/next/intune/user/controller/UserController.java
+++ src/main/java/com/next/intune/user/controller/UserController.java
@@ -2,6 +2,7 @@ package com.next.intune.user.controller;
 
 import com.next.intune.common.api.ApiResult;
 import com.next.intune.user.dto.request.SignInRequestDto;
+import com.next.intune.user.dto.request.SignUpRequestDto;
 import com.next.intune.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -20,6 +21,9 @@ public class UserController {
 
     private final UserService userService;
 
+    /* -----------------------
+       로그인
+       ----------------------- */
     @PostMapping("/sign-in")
     @Operation(summary = "로그인", description = "이메일과 비밀번호을 입력해주세요.")
     public ResponseEntity<ApiResult<?>> signIn(HttpServletResponse response, @Valid @RequestBody SignInRequestDto dto) {
@@ -27,6 +31,19 @@ public class UserController {
         return ResponseEntity.ok(ApiResult.success());
     }
 
+    /* -----------------------
+       회원가입
+       ----------------------- */
+    @PostMapping("/sign-up")
+    @Operation(summary = "회원가입", description = "항목을 입력해주세요.")
+    public ResponseEntity<ApiResult<?>> signUp(HttpServletResponse response, @Valid @RequestBody SignUpRequestDto dto) {
+        userService.signUp(response, dto);
+        return ResponseEntity.ok(ApiResult.success());
+    }
+
+    /* -----------------------
+       회원 탈퇴
+       ----------------------- */
     @PostMapping("/remove")
     @Operation(summary = "유저 탈퇴")
     public ResponseEntity<ApiResult<?>> removeMember(HttpServletRequest request) {
